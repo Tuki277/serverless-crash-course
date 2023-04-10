7846bbd9dc2080876c9c1bf6c4336b0e44340a94
Tran Huy <huybeos2707@gmail.com>
2023-04-10 09:48:36 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3000;
+const host = process.env.HOST
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get('/check-health', (req, res) => {
+  res.json({'message': 'ok'});
+})
+
+/* Error handler middleware */
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({'message': err.message});
+  
+  return;
+});
+
+app.listen(port, host, () => {
+  console.log(`Example app listening at http://${host}:${port}`)
+});
